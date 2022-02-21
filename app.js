a2d559272a4f307dbd1d8aa8302d91cc3ef735fc
Mati <matiasnoe2002@gmail.com>
2022-02-21 16:29:27 -0300

    Probanddo

--- app.js
+++ app.js
@@ -11,3 +11,5 @@ app.get ("/", (req,res) => res.sendFile (path.resolve(__dirname, "./views/home.h
 app.use(express.static(path.resolve(__dirname, './public')));
 app.get ("/register", (req,res) => res.sendFile (path.resolve(__dirname, "./views/register.html")))
 app.get ("/login", (req,res) => res.sendFile (path.resolve(__dirname, "./views/login.html")))
+
+// Hola miundo
